fd9a8708a48250464cecbdef6f99c9a9d30f5c14
Oleg Girko <ol@infoserver.lv>
2018-04-12 23:36:10 +0100

    Add a new field containing pointer to network backend to CNetAddr.
    This is the first stem to abstract away addressing.
    The goal is to make addressing backend-specific.
    16-byte ip field will eventually become a backend-specific raw data
    that will be interpreted by network backend.
    
    Default constructor is still available and uses default backend,
    that is CNetBackendTcp (existing protocol using TCP connections).
    
    A new constructor added to CNetAddr and all classes that derive from it
    that allows to specify backend explicitly.
    
    Currently new network backend field is almost unused
    (used only in few asserts).
    Moving all network operations to network backend is planned soon.
    
    Signed-off-by: Oleg Girko <ol@infoserver.lv>

--- src/protocol.h
+++ src/protocol.h
@@ -312,6 +312,7 @@ class CAddress : public CService
 {
 public:
     CAddress();
+    CAddress(const CNetBackend& netbackend);
     explicit CAddress(CService ipIn, ServiceFlags nServicesIn);
 
     void Init();
